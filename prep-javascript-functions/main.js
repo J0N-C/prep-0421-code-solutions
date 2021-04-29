955d9b96d36c42db66abea71f3fc08a76cc6bab7
Jonathan Chiang <jc91765@gmail.com>
2021-04-28 20:47:48 -0700

    convertHourstoMinutes prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,9 @@ function addTwoNumbers(num1, num2) {
 
 var addTwoNumbersResult = addTwoNumbers(2, 2);
 console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHourstoMinutes(hours) {
+  return hours * 60;
+}
+var twoHours = convertHourstoMinutes(2);
+console.log('convertHourstoMinutes Exercise:', twoHours);
